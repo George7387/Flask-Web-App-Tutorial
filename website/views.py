9396b908724a5aa3d0fc56f9a50a345d4a00c5cb
George7387 <99222911+George7387@users.noreply.github.com>
2023-05-10 09:57:27 +1200

    Update views.py

--- website/views.py
+++ website/views.py
@@ -27,7 +27,7 @@ def home():
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
+    noteId = note['noteId'] 
     note = Note.query.get(noteId)
     if note:
         if note.user_id == current_user.id:
